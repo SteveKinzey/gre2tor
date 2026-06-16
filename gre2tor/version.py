ddba740209770bf1ee662ef4e7b4bf5d1a0172f2
Stephen J. Kinzey, Ph.D. <steve@sk-america.com>
2026-06-15 22:24:59 -0700

    Bump version to 0.1.1

--- gre2tor/version.py
+++ gre2tor/version.py
@@ -1,3 +1,3 @@
-APP_VERSION = "0.1.0"
+APP_VERSION = "0.1.1"
 GITHUB_RELEASES_URL = "https://github.com/SteveKinzey/gre2tor/releases/latest"
 GITHUB_LATEST_RELEASE_API = "https://api.github.com/repos/SteveKinzey/gre2tor/releases/latest"
